a6cc16593df38872aba51f6357291016e6881c1d
Chen Liang <87241041+Chen2357@users.noreply.github.com>
2025-07-26 16:34:12 +0800

    Export the Seven submodules by default instead

--- Sphere.lean
+++ Sphere.lean
@@ -1,11 +1,18 @@
 import Sphere.Utilities
 import Sphere.Basic
 
-import Sphere.Basis
-import Sphere.Duality
-import Sphere.Differential
-import Sphere.Lie
+-- import Sphere.Basis
+-- import Sphere.Duality
+-- import Sphere.Differential
+-- import Sphere.Lie
 
-import Sphere.Operator
-import Sphere.Flatness
-import Sphere.Quantization
+-- import Sphere.Operator
+-- import Sphere.Flatness
+-- import Sphere.Quantization
+
+import Sphere.Seven.Basis
+import Sphere.Seven.Fundamental
+import Sphere.Seven.Alpha
+import Sphere.Seven.DifferentialAlpha
+import Sphere.Seven.Lie
+import Sphere.Seven.Reeb
