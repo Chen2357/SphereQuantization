2c6f287b07918209a6bc6826482e710472519ea4
Chen Liang <87241041+Chen2357@users.noreply.github.com>
2025-08-03 10:20:01 +0800

    Add Lie bracket relations with Reeb

--- Sphere/Seven/Lie.lean
+++ Sphere/Seven/Lie.lean
@@ -1,5 +1,6 @@
 import Sphere.Seven.Alpha
 import Sphere.Seven.DifferentialAlpha
+import Sphere.Seven.Reeb
 
 @[simp]
 lemma L_ξ_k1_α : L (ξ k1) α = 0 := by
@@ -47,4 +48,73 @@ lemma L_ξ_p2_α : L (ξ p2) α = 0 := by
 @[simp]
 lemma L_ξ_p3_α : L (ξ p3) α = 0 := by
   simp [L, ι_ξ_p3_α, ι_ξ_p3_dα, d_mul, subalgebra_smul_eq_cast_mul]
+  abel
+
+@[simp]
+lemma lie_ξ_j1_R : ⁅ξ j1, R⁆ = 0 := by
+  simp only [ξ_j1, R, lie_smul, smul_lie]
+  simp only [lie_add, add_lie, lie_sub, sub_lie, lie_Λ0smul, Λ0smul_lie]
+  simp [smul_smul]
+  abel
+
+@[simp]
+lemma lie_ξ_j2_R : ⁅ξ j2, R⁆ = 0 := by
+  simp only [ξ_j2, R, lie_smul, smul_lie]
+  simp only [lie_add, add_lie, lie_sub, sub_lie, lie_Λ0smul, Λ0smul_lie]
+  simp [smul_smul]
+  abel
+
+@[simp]
+lemma lie_ξ_j3_R : ⁅ξ j3, R⁆ = 0 := by
+  simp only [ξ_j3, R, lie_smul, smul_lie]
+  simp only [lie_add, add_lie, lie_sub, sub_lie, lie_Λ0smul, Λ0smul_lie]
+  simp [smul_smul]
+
+@[simp]
+lemma lie_ξ_k1_R : ⁅ξ k1, R⁆ = 0 := by
+  simp only [ξ_k1, R, lie_smul, smul_lie]
+  simp only [lie_add, add_lie, lie_sub, sub_lie, lie_Λ0smul, Λ0smul_lie]
+  simp [smul_smul]
+  abel
+
+@[simp]
+lemma lie_ξ_k2_R : ⁅ξ k2, R⁆ = 0 := by
+  simp only [ξ_k2, R, lie_smul, smul_lie]
+  simp only [lie_add, add_lie, lie_sub, sub_lie, lie_Λ0smul, Λ0smul_lie]
+  simp [smul_smul]
+  abel
+
+@[simp]
+lemma lie_ξ_k3_R : ⁅ξ k3, R⁆ = 0 := by
+  simp only [ξ_k3, R, lie_smul, smul_lie]
+  simp only [lie_add, add_lie, lie_sub, sub_lie, lie_Λ0smul, Λ0smul_lie]
+  simp [smul_smul]
+  abel
+
+@[simp]
+lemma lie_ξ_p0_R : ⁅ξ p0, R⁆ = 0 := by
+  simp only [ξ_p0, R, lie_smul, smul_lie]
+  simp only [lie_add, add_lie, lie_sub, sub_lie, lie_Λ0smul, Λ0smul_lie]
+  simp [smul_smul, lie_Λ0smul]
+  abel
+
+@[simp]
+lemma lie_ξ_p1_R : ⁅ξ p1, R⁆ = 0 := by
+  simp only [ξ_p1, R, lie_smul, smul_lie]
+  simp only [lie_add, add_lie, lie_sub, sub_lie, lie_Λ0smul, Λ0smul_lie]
+  simp [smul_smul, lie_Λ0smul]
+  abel
+
+@[simp]
+lemma lie_ξ_p2_R : ⁅ξ p2, R⁆ = 0 := by
+  simp only [ξ_p2, R, lie_smul, smul_lie]
+  simp only [lie_add, add_lie, lie_sub, sub_lie, lie_Λ0smul, Λ0smul_lie]
+  simp [smul_smul, lie_Λ0smul]
+  abel
+
+@[simp]
+lemma lie_ξ_p3_R : ⁅ξ p3, R⁆ = 0 := by
+  simp only [ξ_p3, R, lie_smul, smul_lie]
+  simp only [lie_add, add_lie, lie_sub, sub_lie, lie_Λ0smul, Λ0smul_lie]
+  simp [smul_smul, lie_Λ0smul]
   abel
