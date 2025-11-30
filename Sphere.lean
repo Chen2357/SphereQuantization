0c4d10a471eade7e5d49435cfb47372ccf3653c3
Chen Liang <87241041+Chen2357@users.noreply.github.com>
2025-11-29 22:00:46 -0800

    Change imports

--- Sphere.lean
+++ Sphere.lean
@@ -1,23 +1,23 @@
 import Sphere.Utilities
 import Sphere.Basic
 
--- import Sphere.Basis
--- import Sphere.Duality
--- import Sphere.Differential
--- import Sphere.Lie
+import Sphere.Basis
+import Sphere.Duality
+import Sphere.Differential
+import Sphere.Lie
 
--- import Sphere.Operator
--- import Sphere.Flatness
--- import Sphere.Quantization
+import Sphere.Operator
+import Sphere.Flatness
+import Sphere.Quantization
 
-import Sphere.Seven.Basis
+-- import Sphere.Seven.Basis
 
-import Sphere.Seven.Fundamental
-import Sphere.Seven.Commutation
-import Sphere.Seven.LieFundamental
+-- import Sphere.Seven.Fundamental
+-- import Sphere.Seven.Commutation
+-- import Sphere.Seven.LieFundamental
 
-import Sphere.Seven.Alpha
-import Sphere.Seven.DifferentialAlpha
+-- import Sphere.Seven.Alpha
+-- import Sphere.Seven.DifferentialAlpha
 
-import Sphere.Seven.Reeb
-import Sphere.Seven.Lie
+-- import Sphere.Seven.Reeb
+-- import Sphere.Seven.Lie
